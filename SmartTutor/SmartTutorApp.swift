57423a7d4cd205148cef721cf207c69bfdf6067f
Abhinav <gs.abhinav@gmail.com>
2026-01-30 13:57:09 +0530

    Initial Commit

---
+++ SmartTutor/SmartTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartTutorApp.swift
+//  SmartTutor
+//
+//  Created by Abhinav Gupta on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SmartTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
